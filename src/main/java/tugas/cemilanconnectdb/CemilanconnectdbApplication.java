3e0b050f3b75833987794a320baf67a5fa6c8283
yamz-11 <wiratamajrz@gmail.com>
2022-10-17 21:19:54 +0700

    cemilan tugas koneksi DB

---
+++ src/main/java/tugas/cemilanconnectdb/CemilanconnectdbApplication.java
@@ -0,0 +1,13 @@
+package tugas.cemilanconnectdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CemilanconnectdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CemilanconnectdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tugas/cemilanconnectdb/CemilanconnectdbApplicationTests.java
@@ -0,0 +1,13 @@
+package tugas.cemilanconnectdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CemilanconnectdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
